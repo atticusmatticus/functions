63e3bdb0f4d975066c53307ae5cca43eedb2550d
atticusmatticus <20863400+atticusmatticus@users.noreply.github.com>
2019-03-15 18:14:13 -0600

    Added `tridag` with minor changes from NR. Added `bilin_interp`. Minor alterations to `spline` and `splint`. Major revision of `bicubic_int`. Added symmetric versions of both `spline` and `tridag` to be used with symmetric functions and `symm_splint` which interpolates the symmetric spline/tridag arrays.

--- functions.f90
+++ functions.f90
@@ -54,59 +54,170 @@ contains
 	end function rotate_z
 
 
-	! NOTE: the following spline and bicubic interpolation subroutines are adopted from Numerical Recipies in FORTRAN 2nd Ed. 
+	! NOTE: the following tridiag, spline, and bicubic interpolation subroutines are adopted from Numerical Recipies in FORTRAN 2nd Ed. 
+	! note: all lines marked with the 'xxx' flag are changes to the original NR code.
+
+	! Solves for a vector u(1:n) of length n the tridiagonal linear set given by equation (2.4.1). a(1:n), b(1:n), c(1:n), and r(1:n)
+	! are input vectors and are not modiﬁed. Parameter: NMAX is the maximum expected value of n.
+	subroutine tridag(a,b,c,r,u,n)
+		implicit none
+		integer			:: n	!, NMAX
+		real(kind=dp)	:: a(n), b(n), c(n), r(n), u(n)
+!		parameter		:: (NMAX=500)
+		integer			:: j
+		real(kind=dp)	:: bet, gam(n)	!, gam(NMAX)	! One vector of workspace, gam is needed.
+
+		if ((b(1).lt.1d-6) .and. (b(1).gt.-1d-6)) then	! ie. it's equal to zero xxx
+			! If this happens then you should rewrite your equations as a set of order N−1, with u2 trivially eliminated.
+			write(*,*) 'ERROR: tridag: rewrite equations'
+			error stop
+		end if
+
+		bet = b(1)
+		u(1) = r(1)/bet
+
+		! Decomposition and forward substitution.
+		do j = 2, n
+			gam(j) = c(j-1)/bet
+			bet = b(j) - a(j)*gam(j)
+			if ((bet.lt.1d-6) .and. (bet.gt.-1d-6)) then	! ie. it's equal to zero xxx
+				write(*,*) 'ERROR: tridag failed'	! Algorithm fails; see below.
+				error stop
+			end if
+			u(j) = (r(j) - a(j)*u(j-1))/bet
+		end do
+
+		! Backsubstitution.
+		do j = n-1, 1, -1
+			u(j) = u(j) - gam(j+1)*u(j+1)
+		end do
+
+	end subroutine tridag
+
+
+	! Solves for a vector u(1:n) of length n the tridiagonal linear set given by equation (2.4.1). a(1:n), b(1:n), c(1:n), and r(1:n)
+	! are input vectors and are not modiﬁed. Parameter: NMAX is the maximum expected value of n.
+	subroutine symm_tridag(dx,y,n, u)
+		implicit none
+		integer			:: n
+		real(kind=dp)	:: dx, y(n), u(n)
+		! locally defined
+		real(kind=dp)	:: a(n), b(n), c(n), r(n)
+		integer			:: j
+		real(kind=dp)	:: bet, gam(n)
+
+		! symmetric system
+		a(1) = 0_dp
+		a(2:n) = dx/real(6,dp)
+
+		b(1) = 5*dx/real(6,dp)
+		b(2:n-1) = 2*dx/real(3,dp)
+		b(n) = b(1)
+
+		c(1:n-1) = dx/real(6,dp)
+		c(n) = 0_dp
+
+		r(1) = (y(2)-y(1))/dx
+		do j = 2, n-1
+			r(j) = ((y(j+1)-y(j)) - (y(j)-y(j-1))) / dx
+		end do
+		r(n) = -(y(n)-y(n-1))/dx
+
+		bet = b(1)
+		u(1) = r(1)/bet
+
+		! Decomposition and forward substitution.
+		do j = 2, n
+			gam(j) = c(j-1)/bet
+			bet = b(j) - a(j)*gam(j)
+			if ((bet.lt.1d-6) .and. (bet.gt.-1d-6)) then	! ie. it's equal to zero xxx
+				write(*,*) 'ERROR: symm_tridag failed'	! Algorithm fails; see below.
+				error stop
+			end if
+			u(j) = (r(j) - a(j)*u(j-1))/bet
+		end do
+
+		! Backsubstitution.
+		do j = n-1, 1, -1
+			u(j) = u(j) - gam(j+1)*u(j+1)
+		end do
+
+	end subroutine symm_tridag
+
 
 	! Given arrays x(1:n) and y(1:n) containing a tabulated function, i.e., y_i = f(x_i), with x_1 < x_2 < ... < x_n, and given
 	! values 'yp1' and 'ypn' for the first derivative of the interpolating function at points 1 and n, respectively, this
 	! routine returns an array y2(1:n) of length n which contains the second derivatives of the interpolting function at the
 	! tabulated points x_i. If 'yp1' and/or 'ypn' are equal to 1*10^30 or larger, the routine is signaled to set the
 	! corresponding boundary condition for a natural spline, with zero second derivative on that boundary.
-	subroutine spline( x, y, n, yp1, ypn, y2 )
+	subroutine spline( x, y, nmin, n, yp1, ypn, y2 ) !xxx
 		implicit none
+		integer 				:: nmin	!xxx
 		integer 				:: n
 		real(kind=dp)		:: yp1, ypn, x(n), y(n), y2(n)
 		! Locally defined variables
 		integer				:: i, k
 		real(kind=dp)		:: p, qn, sig, un, u(n)
 
-		if (yp1 .gt. .99e30) then	! the lower boundary condition is set either to be "natural"
-			y2(1) = 0_dp
-			u(1) = 0_dp
-		else						! or else to have a specified first derivative.
-			y2(1) = -0.5_dp
-			u(1) = ( 3_dp / (x(2)-x(1)) ) * ( (y(2)-y(1)) / (x(2)-x(1)) - yp1 )
-		end if
+		y2(nmin) = -0.5_dp	!xxx
+		u(nmin) = ( 3_dp / (x(nmin+1)-x(nmin)) ) * ( (y(nmin+1)-y(nmin)) / (x(nmin+1)-x(nmin)) - yp1 ) !xxx
 
 		! this is the decomposition loop of the tridiagonal algorithm. y2 and u are used for temporary storage of the decomposed
 		! factors.
-		do i = 2, n-1
+		do i = nmin+1, n-1	!xxx
 			sig = ( x(i)-x(i-1) ) / ( x(i+1)-x(i-1) )
 			p = sig * y2(i-1) + 2
 			y2(i) = (sig-1_dp) / p
-			u(i) = ( 6_dp * ( (y(i+1)-y(i)) / (x(i+1)-x(i)) - (y(i)-y(i-1)) / (x(i)-x(i-1)) ) &
-				& / (x(i+1)-x(i-1)) - sig*u(i-1) ) / p
+			u(i) = ( 6_dp * ( (y(i+1)-y(i)) / (x(i+1)-x(i)) - (y(i)-y(i-1)) / (x(i)-x(i-1)) ) / (x(i+1)-x(i-1)) - sig*u(i-1) ) / p
 		end do
 
-		if (ypn .gt. .99e30) then	! the upper boundary condition is set either to be "natural"
-			qn = 0_dp
-			un = 0_dp
-		else						! or else to have a specific first derivative.
-			qn = 0.5_dp
-			un = ( 3_dp / (x(n)-x(n-1)) ) * ( ypn - (y(n)-y(n-1)) / (x(n)-x(n-1)) )
-		end if
+		qn = 0.5_dp
+		un = ( 3_dp / (x(n)-x(n-1)) ) * ( ypn - (y(n)-y(n-1)) / (x(n)-x(n-1)) )
 
 		y2(n) = ( un - qn * u(n-1) ) / ( qn * y2(n-1) + 1_dp )
-		do k = n-1, 1, -1		! this is the backsubstitution loop of the tridiagonal algorithm.
+		do k = n-1, nmin, -1		! this is the backsubstitution loop of the tridiagonal algorithm. !xxx
 			y2(k) = y2(k) * y2(k+1) + u(k)
 		end do
 
 	end subroutine spline
 
 
+	! spline of a symmetric function like f(x)=cos(x) with equal spacing of x values, ie. constant dx
+	subroutine symm_spline( dx, y, nmin, n, y2 ) !xxx dx instead of x-array
+		implicit none
+		integer 				:: nmin	!xxx
+		integer 				:: n
+		real(kind=dp)		:: dx, y(n), y2(n)
+		! Locally defined variables
+		integer				:: i, k
+		real(kind=dp)		:: p, qn, un, u(n)
+
+		y2(nmin) = -0.2_dp	!xxx symmetric system
+		u(nmin) = ( 6_dp*(y(nmin+1)-y(nmin)) / (5_dp*dx**2) )	!xxx symmetric system
+
+		! this is the decomposition loop of the tridiagonal algorithm. y2 and u are used for temporary storage of the decomposed
+		! factors.
+		do i = nmin+1, n-1	!xxx replaced all 'sig' values with 1/2
+			p = y2(i-1)/2_dp + 2
+			y2(i) = (-0.5_dp) / p
+			u(i) = ( (3_dp * (y(i+1)-2_dp*y(i)+y(i-1)) / dx**2) - u(i-1)/2_dp ) / p
+		end do
+
+		qn = 0.2_dp	!xxx symmetric system
+		un = 6_dp*(y(n-1)-y(n)) / (5_dp*dx**2)	!xxx symmetric system
+
+		y2(n) = ( un - qn * u(n-1) ) / ( qn * y2(n-1) + 1_dp )
+		do k = n-1, nmin, -1		! this is the backsubstitution loop of the tridiagonal algorithm. !xxx
+			y2(k) = y2(k) * y2(k+1) + u(k)
+		end do
+
+	end subroutine symm_spline
+
+
 	! Given the arrays xa(1:n) and ya(1:n) of length n, which tabulate a function (with the xa_i's in order), and given the
-	! array y2a(1:n), which is the output from 'spline' above, and given a value of x, this routine returns a cubic-spline
-	! interpolated value y.
-	subroutine splint( xa, ya, y2a, n, x, y )
+	! array y2a(1:n), which is the output from 'spline' above (2nd derivtive of ya), and given a value of x, this routine returns a
+	! cubic-spline interpolated value y.
+	subroutine splint(xa,ya,y2a,n,x, y)
 		implicit none
 		integer			:: n
 		real(kind=dp)	:: xa(n), ya(n), y2a(n), x, y
@@ -128,10 +239,12 @@ contains
 				klo = k
 			end if
 		end do
+
 		! khi and klo now bracket the input value of x.
 		h = xa(khi)-xa(klo)
 		if (h .eq. 0_dp) then
 			write(*,*) 'ERROR: bad xa input in subroutine: splint'	! the xa's must be distinct.
+			error stop
 		end if
 		! cubic spline polynomial is now evaluated.
 		a = (xa(khi)-x)/h
@@ -141,8 +254,51 @@ contains
 	end subroutine splint
 
 
-	! Use the 16 surrounding splined points to take the vertical, horizontal, and cross derivatives and then calls bcuint.
-	subroutine bicubic_int(x1a,x2a,n1,n2,dx1,dx2,x1min,x2min,ya,x1,x2, ansy)!,ansy1,ansy2)
+	subroutine symm_splint(xa,dx,ya,y2a,n,x, y)
+		implicit none
+		integer			:: n
+		real(kind=dp)	:: xa(n), dx, ya(n), y2a(n), x, y
+		! Locally defined variables
+		integer			:: k, khi, klo
+		real(kind=dp)	:: a, b
+
+		! We will find the right place in the table by means of bisection. This is optimal if sequential calls to this routine
+		! are at random values of x. If sequential calls are in order, and closely spaced, one would do better to store previous
+		! values of klo and khi and test if they remain appropriate on the next call.
+		klo = 0
+		khi = n+1
+		do
+			if (khi-klo .eq. 1) exit ! do while loop with this exit statement instead of a goto like Numerical Recipies has.
+			k = (khi+klo)/2
+			if (xa(k) .gt. x) then
+				khi = k
+			else
+				klo = k
+			end if
+		end do
+
+		! khi and klo now bracket the input value of x.
+		! cubic spline polynomial is now evaluated.
+		if (klo.eq.0) then
+			a = (xa(khi)-x)/dx
+			b = 1-a
+			y = a*ya(khi) + b*ya(khi) + ((a**3-a) * y2a(khi) + (b**3-b) * y2a(khi)) * (dx**2)/6_dp
+		elseif (khi.eq.n+1) then
+			b = (x-xa(klo))/dx
+			a = 1-b
+			y = a*ya(klo) + b*ya(klo) + ((a**3-a) * y2a(klo) + (b**3-b) * y2a(klo)) * (dx**2)/6_dp
+		else
+			a = (xa(khi)-x)/dx
+			b = (x-xa(klo))/dx
+			y = a*ya(klo) + b*ya(khi) + ((a**3-a) * y2a(klo) + (b**3-b) * y2a(khi)) * (dx**2)/6_dp
+		end if
+
+	end subroutine symm_splint
+
+
+	! Use the 16 surrounding grid points to take the x1, x2, and cross derivatives using centered differencing and then calls bcuint.
+	! However, if all of the grid point values are below a certain cutoff (-10**4) then call bilinear interpolation instead.
+	subroutine bicubic_int(cut, x1a,x2a,n1,n2,dx1,dx2,x1min,x2min,ya,x1,x2, ansy)!,ansy1,ansy2)
 		implicit none
 		integer			:: n1, n2
 		real(kind=dp)	:: x1a(n1), x2a(n2), ya(4,0:n1+1, 0:n2+1), dx1, dx2, x1min, x2min, x1, x2	! input
@@ -151,7 +307,7 @@ contains
 		! locally defined
 		integer								:: lj, lk, uj, uk, j, k
 		real(kind=dp),dimension(4)		:: y, y1, y2, y12
-		real(kind=dp)						:: ansy, ansy1, ansy2
+		real(kind=dp)						:: cut, ansy, ansy1, ansy2
 
 		! todo: first need to identify what the 4 points surrounding the desired point are. These are points 1-4 circled in NR p.117
 		! todo: pass this routine the actual variable value for one of the solutes to get a float_index.
@@ -159,32 +315,40 @@ contains
 		lk = floor((x2-x2min)/dx2 + 0.5_dp)
 		uj = lj+1
 		uk = lk+1
-		! todo: define points 1-4 from NR. 1 = (lj,lk); 2 = (uj,lk); 3 = (uj,uk); 4 = (lj,uk). Into 1D arrays as needed by the other
-		! subs and then call bcuint with all the arrays arranged appropriately for it.
-		! the function
-		y(1) = ya(1,lj,lk)
-		y(2) = ya(1,uj,lk)
-		y(3) = ya(1,uj,uk)
-		y(4) = ya(1,lj,uk)
-		! gradient along x1
-		y1(1) = ya(2,lj,lk)
-		y1(2) = ya(2,uj,lk)
-		y1(3) = ya(2,uj,uk)
-		y1(4) = ya(2,lj,uk)
-		! gradient along x2
-		y2(1) = ya(3,lj,lk)
-		y2(2) = ya(3,uj,lk)
-		y2(3) = ya(3,uj,uk)
-		y2(4) = ya(3,lj,uk)
-		! cross derivative
-		y12(1) = ya(4,lj,lk)
-		y12(2) = ya(4,uj,lk)
-		y12(3) = ya(4,uj,uk)
-		y12(4) = ya(4,lj,uk)
+		! todo: perhaps after calculating the indicies I could ask if the yvalues are less than or greater than -10**4 and then either
+		! proceed with the bicubic interp or do bilinear interp.
+		! Interpolation cuttoff:
+		if ((ya(1,lj,lk).le.cut).and.(ya(1,uj,lk).le.cut).and.(ya(1,lj,uk).le.cut).and.(ya(1,uj,uk).le.cut)) then
+			! Note: if below the cutoff then bilinear interpolation
+			call bilin_interp(x1a,x2a,n1,n2,dx1,dx2,x1min,x2min,ya(1,:,:),x1,x2, ansy)
+		else
+			! Note: if above the cutoff then bicubic interpolation
+			! Define points 1-4 from NR. 1 = (lj,lk); 2 = (uj,lk); 3 = (uj,uk); 4 = (lj,uk). Into 1D arrays as needed by the other subs
+			! and then call bcuint with all the arrays arranged appropriately for it.
+			y(1) = ya(1,lj,lk)
+			y(2) = ya(1,uj,lk)
+			y(3) = ya(1,uj,uk)
+			y(4) = ya(1,lj,uk)
+			! gradient along x1
+			y1(1) = ya(2,lj,lk)
+			y1(2) = ya(2,uj,lk)
+			y1(3) = ya(2,uj,uk)
+			y1(4) = ya(2,lj,uk)
+			! gradient along x2
+			y2(1) = ya(3,lj,lk)
+			y2(2) = ya(3,uj,lk)
+			y2(3) = ya(3,uj,uk)
+			y2(4) = ya(3,lj,uk)
+			! cross derivative
+			y12(1) = ya(4,lj,lk)
+			y12(2) = ya(4,uj,lk)
+			y12(3) = ya(4,uj,uk)
+			y12(4) = ya(4,lj,uk)
 
-		! todo: build in a way of handling cosTh(1) and cosTh(2) and multiplying them without having to call bicubic_int, (the
-		! current subroutine), twice.
-		call bcuint(y,y1,y2,y12,x1a(lj),x1a(uj),x2a(lk),x2a(uk),x1,x2, ansy,ansy1,ansy2)
+			! todo: build in a way of handling cosTh(1) and cosTh(2) and multiplying them without having to call bicubic_int, (the
+			! current subroutine), twice.
+			call bcuint(y,y1,y2,y12,x1a(lj),x1a(uj),x2a(lk),x2a(uk),x1,x2, ansy,ansy1,ansy2)
+		end if
 
 	end subroutine bicubic_int
 
@@ -259,5 +423,73 @@ contains
 		end do
 		return
 	end subroutine bcucof
+
+
+	! bilinearly interpolate
+	subroutine bilin_interp(x1a,x2a,nx1,nx2,dx1,dx2,x1Min,x2Min,ya,x1,x2, ansy)
+		implicit none
+		! input/output data
+		integer				:: nx1, nx2
+		real(kind=dp)		:: x1a(nx1), x2a(nx2), dx1, dx2, x1Min, x2Min, ya(0:nx1+1,0:nx2+1), x1, x2
+		! locally defined data
+		integer				:: i, j, ix1l, ix1u, ix2l, ix2u
+		real(kind=dp)		:: f_index, x1l, x1u, x2l, x2u, x1d, x2d, ansy, x1Int, x2Int
+
+		! x1
+		f_index = (x1 - x1Min) / dx1 + 0.5_dp ! take into account half-bin positions
+		ix1l = floor(f_index) ! get flanking r indicies
+		if (ix1l .ge. nx1) then
+			ix1l = nx1
+			ix1u = nx1
+		else if (ix1l .lt. 1) then
+			ix1l = 1
+			ix1u = 1
+		else
+			ix1u = ix1l + 1
+		end if
+		x1l = x1a(ix1l)
+		x1u = x1a(ix1u)
+		if ((x1 .lt. x1l) .or. (x1 .gt. x1u)) then
+			x1Int = x1l ! when x1 is outside the bounds it gets set to x1l=x1u.
+		else
+			x1Int = x1
+		end if
+
+		! x2
+		f_index = (x2 - x2Min) / dx2 + 0.5_dp ! so index values start at 1
+		ix2l = floor(f_index) ! get flanking x2 indicies
+		if (ix2l .ge. nx2) then
+			ix2l = nx2
+			ix2u = nx2
+		else if (ix2l .lt. 1) then
+			ix2l = 1
+			ix2u = 1
+		else
+			ix2u = ix2l + 1
+		end if
+		x2l = x2a(ix2l)
+		x2u = x2a(ix2u)
+		if ((x2 .lt. x2l) .or. (x2 .gt. x2u)) then
+			x2Int = x2l
+		else
+			x2Int = x2
+		end if
+
+		! Note: set fractional distances
+		if ( ix1l .eq. ix1u) then ! if x1u=x1l then x1d would become a NaN.
+			x1d = 1_dp
+		else
+			x1d = (x1Int-x1l)/(x1u-x1l)
+		end if
+		if ( ix2l .eq. ix2u ) then ! if x2u=x2l then x2d would become a NaN.
+			x2d = 1_dp
+		else
+			x2d = (x2Int-x2l)/(x2u-x2l)
+		end if
+
+		ansy = (ya(ix1l,ix2l)*(1-x1d) + ya(ix1u,ix2l)*x1d)*(1-x2d) + (ya(ix1l,ix2u)*(1-x1d) + &
+			& ya(ix1u,ix2u)*x1d)*x2d
+
+	end subroutine bilin_interp
 
 end module functions
