fe9f5cec64ecb51be4b08dd58d1ede68be81582d
Max Mattson <20863400+atticusmatticus@users.noreply.github.com>
2019-03-15 18:24:18 -0600

    Precision now generated in a way that will be consistent between compilers.

--- precision.f90
+++ precision.f90
@@ -4,7 +4,7 @@
 ! kind 8=double 8 byte float
 ! kind 16=quadruple 16 byte float
 module prec
-	integer, parameter	:: sp = 4	!kind(1.0)
-	integer, parameter	:: dp = 8	!kind(1.0d0)
-	integer, parameter	:: qp = 16
+	integer, parameter :: sp = kind(1.0)
+	integer, parameter :: dp = selected_real_kind(2*precision(1.0_sp))
+	integer, parameter :: qp = selected_real_kind(2*precision(1.0_dp))
 end module prec
